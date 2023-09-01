8b8f16bdbba80fbfcad57587bf067aecfb72e096
FlorianEECKHOUT <97728089+MelvacI@users.noreply.github.com>
2023-09-02 01:30:33 +0200

    add coming soon

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -7,7 +7,7 @@ export default function MainContent() {
     return (
         <div id="maint-content" className="wrap">
             <h1 className="font-2">
-            
+                Coming soon ...<span className="cursor">|</span>
             </h1>
         </div>
     );
