75dbaed59e1b9dd8dd0d173d809c53a709dc0bc0
FlorianEECKHOUT <97728089+MelvacI@users.noreply.github.com>
2023-09-04 22:34:47 +0200

    menu UX

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,30 @@ import Loader from './Loader';
 import MainContent from './MainContent';
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const [glitch, setGlitch] = useState(false);
   const [welcome, setWelcome] = useState(false);
   const [fadeOut, setFadeOut] = useState(false);
 
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     setGlitch(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setGlitch(true);
 
-  //     setTimeout(() => {
-  //       setGlitch(false);
-  //       setWelcome(true);
-  //       setTimeout(() => {
-  //         setFadeOut(true);
-  //         setTimeout(() => {
-  //           setLoading(false);
-  //         }, 1000);
-  //       }, 2000);
-  //     }
-  //       , 1000);
-  //   }, 2000);
-  // }, []);
+      setTimeout(() => {
+        setGlitch(false);
+        setWelcome(true);
+        setTimeout(() => {
+          setFadeOut(true);
+          setTimeout(() => {
+            setLoading(false);
+          }, 1000);
+        }, 2000);
+      }
+        , 1000);
+    }, 2000);
+  }, []);
 
 
   return (
--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -1,61 +1,80 @@
-import { useEffect, useState } from 'react';
-
-// Path: src/Loader.jsx
+import { useEffect, useState, useRef } from 'react';
 
 export default function MainContent() {
+    const [selectedMenuItem, setSelectedMenuItem] = useState('about');
+    const sections = useRef([]);
+
+    const paddingBlockPixels = 160; // La valeur du padding-block en pixels (5rem * 16px)
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollPosition = window.scrollY + paddingBlockPixels; // Ajout du padding-block
+            sections.current.forEach((section, index) => {
+                if (
+                    scrollPosition >= section.offsetTop &&
+                    scrollPosition < section.offsetTop + section.offsetHeight
+                ) {
+                    setSelectedMenuItem(['about', 'xp', 'project', 'skills'][index]);
+                }
+            });
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        // Nettoyer l'écouteur d'événements lorsque le composant se démonte
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    const triggerSelection = (e, menuItem) => {
+        e.preventDefault();
+        const section = document.querySelector(`#${menuItem}`);
+        window.scrollTo(0, section.offsetTop);
+    };
 
 
+    const itemsLabel = {
+        about: 'À propos',
+        xp: 'Expériences',
+        project: 'Projets',
+        skills: 'Compétences'
+    }
 
     return (
         <div id="main-content" className="wrap">
             <section id="description">
-
                 <div id="presentation">
                     <h1>Florian Eeckhout</h1>
                     <h2>Développeur Web -- Freelance</h2>
-                    <p>Je développe des applications web accessibles à tous, dédiées à la transition écologique.</p>
+                    <p>Je développe des applications web accessibles à tous</p>
                 </div>
                 <div id="menu">
                     <ul>
-                        <li><a href="#about">À propos</a></li>
-                        <li><a href="#xp">Expériences</a></li>
-                        <li><a href="#project">Projets</a></li>
-                        <li><a href="#skills">Compétences</a></li>
+                        {['about', 'xp', 'project', 'skills'].map((menuItem) => (
+                            <li key={menuItem} >
+                                <a className={selectedMenuItem === menuItem ? 'selected' : ''} href={`#${menuItem}`} onClick={(e) => triggerSelection(e, menuItem)}>
+                                    {itemsLabel[menuItem]}                                    
+                                </a>
+                            </li>
+                        ))}
                     </ul>
-
                 </div>
-                <div id="socials">
-
-                </div>
+                <div id="socials"></div>
             </section>
 
             <section id="resume">
-                <h1>Ici mes XP</h1>
-                <div id="about">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit quas ad suscipit itaque laboriosam veritatis numquam ea consectetur voluptatem. Adipisci nam at, voluptas in nostrum ratione pariatur sed eius consectetur.
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed voluptatibus beatae earum temporibus architecto asperiores amet, quos, tempore ducimus cumque minus commodi recusandae ullam fuga ea in enim blanditiis corrupti!</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, mollitia necessitatibus alias incidunt, nostrum modi id ullam voluptatem fuga animi quibusdam ut, aliquid sint unde deleniti repellat explicabo. Laborum praesentium provident ex rerum odio cum obcaecati tempora! Alias qui doloribus distinctio facere voluptas, iste quaerat. Alias illum, consequatur maxime non tempore animi, quasi eos quod ullam magni similique libero officia dolorem delectus eligendi totam atque voluptas pariatur doloribus tenetur nulla placeat neque natus dolorum! Pariatur, exercitationem? Unde, cum nemo iusto neque libero odio sequi accusamus assumenda quis magni, ratione officia sit praesentium aspernatur dolores soluta velit dolor ipsam cumque consequatur aliquam quas vel amet laudantium! Et esse vero pariatur consectetur ex nisi amet soluta, magni alias suscipit aspernatur ipsum consequatur dolor earum asperiores tenetur, quam excepturi, architecto atque tempora sequi! Tenetur repudiandae hic magni quisquam eius rerum explicabo harum, autem quasi nemo quos assumenda officia similique voluptatibus voluptates facilis aliquam eum quo? Tempora nesciunt dolor voluptatibus sed corporis, officiis saepe voluptates inventore, molestiae totam doloribus id iste odit mollitia minus incidunt dolores excepturi delectus tenetur at, dignissimos impedit provident illo! Rem quibusdam nesciunt est odio. Eos, earum deleniti. Recusandae hic non accusamus laborum cupiditate sed cumque doloremque doloribus neque. Ducimus!</p>
-                </div>
+                {['about', 'xp', 'project', 'skills'].map((sectionId, index) => (
+                    <article key={sectionId} id={sectionId} ref={(el) => (sections.current[index] = el)}>
+                        <h1>
+                        {itemsLabel[sectionId]}                                    
 
-                <div id="xp">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit quas ad suscipit itaque laboriosam veritatis numquam ea consectetur voluptatem. Adipisci nam at, voluptas in nostrum ratione pariatur sed eius consectetur.
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed voluptatibus beatae earum temporibus architecto asperiores amet, quos, tempore ducimus cumque minus commodi recusandae ullam fuga ea in enim blanditiis corrupti!</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, mollitia necessitatibus alias incidunt, nostrum modi id ullam voluptatem fuga animi quibusdam ut, aliquid sint unde deleniti repellat explicabo. Laborum praesentium provident ex rerum odio cum obcaecati tempora! Alias qui doloribus distinctio facere voluptas, iste quaerat. Alias illum, consequatur maxime non tempore animi, quasi eos quod ullam magni similique libero officia dolorem delectus eligendi totam atque voluptas pariatur doloribus tenetur nulla placeat neque natus dolorum! Pariatur, exercitationem? Unde, cum nemo iusto neque libero odio sequi accusamus assumenda quis magni, ratione officia sit praesentium aspernatur dolores soluta velit dolor ipsam cumque consequatur aliquam quas vel amet laudantium! Et esse vero pariatur consectetur ex nisi amet soluta, magni alias suscipit aspernatur ipsum consequatur dolor earum asperiores tenetur, quam excepturi, architecto atque tempora sequi! Tenetur repudiandae hic magni quisquam eius rerum explicabo harum, autem quasi nemo quos assumenda officia similique voluptatibus voluptates facilis aliquam eum quo? Tempora nesciunt dolor voluptatibus sed corporis, officiis saepe voluptates inventore, molestiae totam doloribus id iste odit mollitia minus incidunt dolores excepturi delectus tenetur at, dignissimos impedit provident illo! Rem quibusdam nesciunt est odio. Eos, earum deleniti. Recusandae hic non accusamus laborum cupiditate sed cumque doloremque doloribus neque. Ducimus!</p>
-
-                </div>
-
-                <div id="project">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit quas ad suscipit itaque laboriosam veritatis numquam ea consectetur voluptatem. Adipisci nam at, voluptas in nostrum ratione pariatur sed eius consectetur.
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, mollitia necessitatibus alias incidunt, nostrum modi id ullam voluptatem fuga animi quibusdam ut, aliquid sint unde deleniti repellat explicabo. Laborum praesentium provident ex rerum odio cum obcaecati tempora! Alias qui doloribus distinctio facere voluptas, iste quaerat. Alias illum, consequatur maxime non tempore animi, quasi eos quod ullam magni similique libero officia dolorem delectus eligendi totam atque voluptas pariatur doloribus tenetur nulla placeat neque natus dolorum! Pariatur, exercitationem? Unde, cum nemo iusto neque libero odio sequi accusamus assumenda quis magni, ratione officia sit praesentium aspernatur dolores soluta velit dolor ipsam cumque consequatur aliquam quas vel amet laudantium! Et esse vero pariatur consectetur ex nisi amet soluta, magni alias suscipit aspernatur ipsum consequatur dolor earum asperiores tenetur, quam excepturi, architecto atque tempora sequi! Tenetur repudiandae hic magni quisquam eius rerum explicabo harum, autem quasi nemo quos assumenda officia similique voluptatibus voluptates facilis aliquam eum quo? Tempora nesciunt dolor voluptatibus sed corporis, officiis saepe voluptates inventore, molestiae totam doloribus id iste odit mollitia minus incidunt dolores excepturi delectus tenetur at, dignissimos impedit provident illo! Rem quibusdam nesciunt est odio. Eos, earum deleniti. Recusandae hic non accusamus laborum cupiditate sed cumque doloremque doloribus neque. Ducimus!</p>
-
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed voluptatibus beatae earum temporibus architecto asperiores amet, quos, tempore ducimus cumque minus commodi recusandae ullam fuga ea in enim blanditiis corrupti!</p>
-                </div>
-
-                <div id="skills">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit quas ad suscipit itaque laboriosam veritatis numquam ea consectetur voluptatem. Adipisci nam at, voluptas in nostrum ratione pariatur sed eius consectetur.
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, mollitia necessitatibus alias incidunt, nostrum modi id ullam voluptatem fuga animi quibusdam ut, aliquid sint unde deleniti repellat explicabo. Laborum praesentium provident ex rerum odio cum obcaecati tempora! Alias qui doloribus distinctio facere voluptas, iste quaerat. Alias illum, consequatur maxime non tempore animi, quasi eos quod ullam magni similique libero officia dolorem delectus eligendi totam atque voluptas pariatur doloribus tenetur nulla placeat neque natus dolorum! Pariatur, exercitationem? Unde, cum nemo iusto neque libero odio sequi accusamus assumenda quis magni, ratione officia sit praesentium aspernatur dolores soluta velit dolor ipsam cumque consequatur aliquam quas vel amet laudantium! Et esse vero pariatur consectetur ex nisi amet soluta, magni alias suscipit aspernatur ipsum consequatur dolor earum asperiores tenetur, quam excepturi, architecto atque tempora sequi! Tenetur repudiandae hic magni quisquam eius rerum explicabo harum, autem quasi nemo quos assumenda officia similique voluptatibus voluptates facilis aliquam eum quo? Tempora nesciunt dolor voluptatibus sed corporis, officiis saepe voluptates inventore, molestiae totam doloribus id iste odit mollitia minus incidunt dolores excepturi delectus tenetur at, dignissimos impedit provident illo! Rem quibusdam nesciunt est odio. Eos, earum deleniti. Recusandae hic non accusamus laborum cupiditate sed cumque doloremque doloribus neque. Ducimus!</p>
-
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed voluptatibus beatae earum temporibus architecto asperiores amet, quos, tempore ducimus cumque minus commodi recusandae ullam fuga ea in enim blanditiis corrupti!</p>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam perspiciatis, eaque incidunt, deleniti modi saepe sed aliquam odio sint illum nam reiciendis explicabo id delectus repudiandae voluptatem inventore nobis numquam.</p>
-                </div>
+                        </h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, mollitia id laborum tenetur earum inventore ab repellat quos sed atque ducimus aut corrupti, nihil fugiat reiciendis exercitationem dolor, obcaecati voluptatum?</p>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium accusantium possimus consequatur unde atque cumque, quos sed repellat fugit in excepturi ipsa esse suscipit odit eum libero rerum cupiditate ullam dolorum minus, nesciunt eaque? Quasi dicta explicabo officiis distinctio vero nemo! Rerum quae architecto nostrum soluta magni, quidem veniam? Harum eaque placeat impedit eveniet dolore quae, est incidunt laboriosam quod voluptatum eligendi accusantium expedita facere reiciendis rerum voluptatibus repudiandae. Obcaecati excepturi reiciendis eaque repellendus, assumenda numquam quo eius dolores aut nobis sunt sint at officiis sequi nam doloribus, dolor eos incidunt quibusdam recusandae velit quos voluptates veniam. Cumque dignissimos architecto voluptas provident incidunt nostrum, officia magnam velit consectetur eos consequuntur maxime nesciunt, reprehenderit quis mollitia iste libero perspiciatis esse consequatur! Repellat magni earum dolorem quas quisquam sit quibusdam minima, ullam sed est laborum ipsa eius omnis exercitationem consequatur, doloribus quaerat corrupti? Eligendi, dolore quasi voluptates voluptatem animi magnam, aut sunt soluta, nihil obcaecati totam. Id ad exercitationem quos recusandae porro? Voluptate eius quam expedita, quisquam illo repellat alias error quasi qui labore voluptatibus, eaque sed laboriosam dolorum hic dolorem, accusantium itaque sequi! Impedit, quis, possimus nesciunt consequuntur alias quas laborum quae inventore eaque ullam at magnam. Accusamus dolorum nobis rerum esse officia quod nihil adipisci, natus fuga necessitatibus. Porro eius asperiores minus ratione magni ducimus debitis, vel quaerat saepe neque? Delectus, maiores soluta. Vel molestias fugiat enim. Sapiente optio magnam quod quasi alias, tempora ad hic laborum maiores dicta ipsa quas. Repellat rem quae fuga! Reprehenderit mollitia sequi molestias laboriosam?</p>
+                    </article>
+                ))}
             </section>
-
         </div>
     );
 }
